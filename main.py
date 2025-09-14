956ef28b3c9f63c257c7ae515fd71ab7a1d36c4b
vanamkarthiknetha <vanamkarthiknetha@gmail.com>
2025-09-14 21:37:19 +0530

    add: getToken route, env file, cors middleware

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
-from typing import Union
+from fastapi import FastAPI, Query
+import os
+from livekit import api
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
+
+load_dotenv()
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return {"status": "success", "message": "Server is up and running in 8000 port"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/getToken")
+def get_token(
+    identity: str = Query(...), name: str = Query(...), room: str = Query(...)
+):
+    """
+    Generate a LiveKit access token for a participant.
+    - `identity`: Unique ID for the participant (e.g., "caller1", "agentA").
+    - `name`: Display name in the LiveKit room.
+    - `room`: Room name to join.
+    """
+
+    LIVEKIT_API_KEY = os.getenv("LIVEKIT_API_KEY")
+    LIVEKIT_API_SECRET = os.getenv("LIVEKIT_API_SECRET")
+
+    if not LIVEKIT_API_KEY or not LIVEKIT_API_SECRET:
+        return {"error": "Missing LiveKit API credentials"}
+
+    # Build access token
+    token = (
+        api.AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET)
+        .with_identity(identity)
+        .with_name(name or identity)
+        .with_grants(api.VideoGrants(room_join=True, room=room))
+    )
+
+    data = {
+        "room": room,
+        "identity": identity,
+        "name": name,
+        "token": token.to_jwt(),
+    }
+
+    return {
+        "success": True,
+        "data": data,
+    }
